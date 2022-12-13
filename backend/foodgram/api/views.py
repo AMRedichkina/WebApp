96d9e64d7385eb81219f596c7b675fb20b8e0085
Ваше Имя <Alexandra@MacBook-Air.local>
2022-12-14 00:31:51 +0100

    bug fixes_v.10

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -68,9 +68,20 @@ class RecipesViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'],
             permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
-        ingredients = Amount_ingredients.objects.all().values_list(
-            'ingredients__name', 'ingredients__measurement_unit',
+        final_list = {}
+        ingredients = Amount_ingredients.objects.filter(
+            recipe__cart__user=request.user).values_list(
+            'ingredient__name', 'ingredient__measurement_unit',
             'amount')
+        for item in ingredients:
+            name = item[0]
+            if name not in final_list:
+                final_list[name] = {
+                    'measurement_unit': item[1],
+                    'amount': item[2]
+                }
+            else:
+                final_list[name]['amount'] += item[2]
         pdfmetrics.registerFont(
             TTFont('Slimamif', 'Slimamif.ttf', 'UTF-8'))
         response = HttpResponse(content_type='application/pdf')
@@ -78,13 +89,16 @@ class RecipesViewSet(viewsets.ModelViewSet):
                                            'filename="shopping_list.pdf"')
         page = canvas.Canvas(response)
         page.setFont('Slimamif', size=24)
-        page.drawString(200, 800, 'Shopping list')
+        page.drawString(200, 800, 'Список ингредиентов')
         page.setFont('Slimamif', size=16)
         height = 750
-        page.drawString(75, height, (ingredients))
+        for i, (name, data) in enumerate(final_list.items(), 1):
+            page.drawString(75, height, (f'<{i}> {name} - {data["amount"]}, '
+                                         f'{data["measurement_unit"]}'))
+            height -= 25
         page.showPage()
         page.save()
-        return response
+        return 
 
     def add_obj(self, model, user, pk):
         obj = model.objects.prefetch_related('user').filter(
