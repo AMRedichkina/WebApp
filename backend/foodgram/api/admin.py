d57f5e28d3bcc56f5e6c27105b5c2165304f9f66
Ваше Имя <Alexandra@MacBook-Air.local>
2022-12-19 01:02:06 +0100

    After review (changed admin.py in api_3_1)

--- backend/foodgram/api/admin.py
+++ backend/foodgram/api/admin.py
@@ -3,9 +3,15 @@ from .models import Ingredients, Recipe, Tag, Amount_ingredients
 from .models import Favorite, Cart
 
 
+class Amount_ingredients_Inline(admin.StackedInline):
+    model = Amount_ingredients
+    extra = 3
+
+
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'count_favorites')
     list_filter = ('author', 'name', 'tags')
+    inlines = [Amount_ingredients_Inline]
 
     def count_favorites(self, obj):
         return obj.favorites.count()
@@ -15,20 +21,14 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'color',)
 
 
-class Amount_ingredients_Inline(admin.StackedInline):
-    model = Amount_ingredients
-    extra = 3
-
-
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit',)
     list_filter = ('name',)
-    inlines = [Amount_ingredients_Inline]
 
 
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Ingredients, IngredientAdmin)
-admin.site.register(Amount_ingredients)
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Amount_ingredients)
 admin.site.register(Cart)
 admin.site.register(Favorite)
